0aca4d2deac8a6f777173c353da3ab56bade7f15
Amanda Tjan <amanda.tjan@gmail.com>
2018-03-16 16:04:36 -0600

    Add in more comments

--- server.js
+++ server.js
@@ -66,40 +66,49 @@ app.post('/api/v1/projects/', (request, response) => {
 app.get('/api/v1/palettes', (request, response) => {
   // select palettes table
   database('palettes').select()
+    // return response status 200 and return json palettes
     .then(palettes => {
       response.status(200).json(palettes);
     })
+    // if there is a server error, return status 500 and error object
     .catch(error => {
       response.status(500).json({ error });
     });
 });
 
 app.post('/api/v1/palettes', (request, response) => {
+  // get palette from request body
   const palette = request.body;
-
+  // look through array and use the elements as the requiredParameter
   for (let requiredParameter of ['name', 'color_1', 'color_2', 'color_3', 'color_4', 'color_5', 'project_id']) {
+    // if missing a required parameter, return status 422 and error message
     if (!palette[requiredParameter]) {
       return response
         .status(422)
         .send({error: `Expected format: { name: <String>, color_1: <String>, color_2: <String>, color_3: <String>, color_4: <String>, color_5: <String>, project_id: <Integer> }. You're missing a "${requiredParameter}" property.`})
     }
   }
-
+  // insert into palettes table based on id
   database('palettes').insert(palette, 'id')
     .then(pal => {
+      // return 201, and object with palette info and id
       response.status(201).json({ ...palette, id: pal[0] })
     })
     .catch(error => {
+      // return error object
       response.status(500).json({ error })
     });
 })
 
 app.get('/api/v1/projects/:id/palettes/', (request, response) => {
+  // palettes table, select rows based on project id pssed in
   database('palettes').where('project_id', request.params.id).select()
     .then(palettes => {
+      // if palettes array has elements, return status 200
       if (palettes.length) {
         response.status(200).json(palettes);
       } else {
+        // return response status 404 and error message with what palette couldn't be found
         response.status(404).json({
           error: `Could not find palettes associated with project id ${request.params.id}`
         });
@@ -110,12 +119,15 @@ app.get('/api/v1/projects/:id/palettes/', (request, response) => {
     });
 });
 
+// get palettes bassed on the id passed in
 app.get('/api/v1/palettes/:id', (request, response) => {
   database('palettes').where('id', request.params.id).select()
     .then(palette => {
+      // if array returned has a palette in it, return status 200
       if (palette.length) {
         response.status(200).json(palette);
       } else {
+        // if nothing, return that it was not found
         response.status(404).json({
           error: `Could not find palette associated with palette id ${request.params.id}`
         });
@@ -127,11 +139,14 @@ app.get('/api/v1/palettes/:id', (request, response) => {
 })
 
 app.delete('/api/v1/palettes/', (request, response) => {
+  // from palettes table where the id matches the one passed in, delete it
   database('palettes').where('id', request.body.id).del()
     .then(palette => {
+      // then response with status 200
       response.status(200).json();
     })
     .catch(error => {
+      // error respond with error 
       response.status(500).json({ error });
     })
 })
