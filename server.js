d2c0b270d66584eb540593a03b45ffb8e06c72b2
Amanda Tjan <amanda.tjan@gmail.com>
2018-03-16 12:55:01 -0600

    Start comments in server file

--- server.js
+++ server.js
@@ -1,54 +1,70 @@
+// importing in express
 const express = require('express');
+// instantiate express
 const app = express();
+// middleware that takes everything in body and parses it to json
 const bodyParser = require('body-parser');
 
 const environment = process.env.NODE_ENV || 'development';
 const configuration = require('./knexfile')[environment];
 const database = require('knex')(configuration);
 
+// in production, PORT will be set for us. If we're running locally, use 3000
 app.set('port', process.env.PORT || 3000);
+// store locally the title of app
 app.locals.title = 'Palette Picker';
-
+// use this middleware throughout app
 app.use(bodyParser.json());
+// pull in index.html
 app.use(express.static('public'))
-
+// root directory get index.html
 app.get('/', (request, response) => {
 
 });
 
+// at the route /api/v1/projects, get this information
 app.get('/api/v1/projects/', (request, response) => {
+  // select table projects in database
   database('projects').select()
+    // return response.status 200 with json projects
     .then(projects => {
       response.status(200).json(projects);
     })
+    // if error, return status 500, and return json error
     .catch(error => {
       response.status(500).json({ error });
     });
 });
 
+// post at route /api/v1/projects/
 app.post('/api/v1/projects/', (request, response) => {
+  // get project from what's passed into the request body
   const project = request.body;
-
+  // look through array and use elements as requiredParameter variable
   for (let requiredParameter of ['name']) {
+    // if there isn't the required param, resturn error status of 422 and senf error message
     if (!project[requiredParameter]) {
       return response
         .status(422)
         .send({ error: `Expected format: { name: <String> }. You're missing a "${requiredParameter}" property.` });
     }
   }
-
+  // in table projects insert project with id
   database('projects').insert(project, 'id')
+    // if okay, send status of 201 and json project object
     .then(proj => {
       const { name } = project;
       
       response.status(201).json({ id: proj[0], name })
     })
+    // catch error and return status 500
     .catch(error => {
       response.status(500).json({ error })
     });
 });
 
 app.get('/api/v1/palettes', (request, response) => {
+  // select palettes table
   database('palettes').select()
     .then(palettes => {
       response.status(200).json(palettes);
